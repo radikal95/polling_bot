05f1d9bcb62ae974ae67a2be3b33e14f1aa8d0b5
Arseny Radushin <radushin.arseny@yandex.ru>
2018-07-04 23:36:49 +0300

    Test

--- run.py
+++ run.py
@@ -9,10 +9,12 @@ db_query = DbQuery()
 bot = telebot.TeleBot(config.token)
 logging.basicConfig(filename="sample.log", level=logging.INFO)
 
+markup = telebot.types.InlineKeyboardMarkup()
+markup.row(telebot.types.InlineKeyboardButton('+1', callback_data=''))
+
 @bot.message_handler(regexp="/test")
 def test(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.row(telebot.types.InlineKeyboardButton('+1', callback_data=message.chat.id))
+
     bot.send_message(message.chat.id, 0,reply_markup=markup)
 
 
@@ -20,13 +22,19 @@ def test(message):
 def callback_inline(call):
     if call.message:
         if call.data:
-            bot.answer_callback_query(call.id, text="Done!")
-            bot.edit_message_text((int(call.message.text)+1),call.message.chat.id,call.message.message_id)
+            # bot.answer_callback_query(call.id, text="Done!")
+            bot.edit_message_text((int(call.message.text)+1),call.message.chat.id,call.message.message_id, reply_markup=markup)
             bot.answer_callback_query(call.id, text="+1")
     pass
             # bot.send_message(call.data, call.message.chat.username'test')
             # for data in call.message.entities[1]:
             #     print((data))
+
+@bot.message_handler(content_types='text')
+def default_answer(message):
+    # bot.send_message(message.chat.id, "You are not authorized")
+    pass
+
 
 
 while True:
