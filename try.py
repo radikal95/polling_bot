2932fe0669a8dd61e91580eee81698073b5e9a77
Arseny Radushin <radushin.arseny@yandex.ru>
2018-07-16 19:53:29 +0300

    Try new result

--- try.py
+++ try.py
@@ -15,4 +15,4 @@ query_result = db_query.execute_query(query)
 for data in query_result.value:
     # print(data)
     # print(data[1])
-    print(bot.get_chat(data[1]))
+    print(bot.get_chat(data[1]).title)
