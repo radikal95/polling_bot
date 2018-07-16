60b7951dbdc53dfbbcfe400694ea606191621687
Arseny Radushin <radushin.arseny@yandex.ru>
2018-07-16 20:43:23 +0300

    Try new result

--- try.py
+++ try.py
@@ -11,8 +11,19 @@ query = """SELECT *
     	        FROM public.polls
     	        ORDER by chat_id ASC, id ASC;"""
 query_result = db_query.execute_query(query)
-
+temp_chat_name = ''
+chat_names = []
 for data in query_result.value:
     # print(data)
     # print(data[1])
-    print(bot.get_chat(data[1]).title)
+    # print(bot.get_chat(data[1]).title)
+    if temp_chat_name ==bot.get_chat(data[1]).title:
+        continue
+    else:
+        chat_names.append(bot.get_chat(data[1]).title)
+        temp_chat_name = bot.get_chat(data[1]).title
+print(chat_names)
+
+
+
+
