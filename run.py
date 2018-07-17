789c1d16dfa25fd7c541348b5d3071d95592e8ea
Arseny Radushin <radushin.arseny@yandex.ru>
2018-07-17 22:10:36 +0300

    Try new result

--- run.py
+++ run.py
@@ -3,12 +3,10 @@ import config
 import time
 from datetime import datetime
 import logging
-import subprocess
-import json
+import zipfile
 import os
 from db_tool import DbQuery
 from openpyxl import Workbook
-
 
 db_query = DbQuery()
 bot = telebot.TeleBot(config.token)
@@ -19,7 +17,8 @@ markup.row(telebot.types.InlineKeyboardButton('1', callback_data='1'),
            telebot.types.InlineKeyboardButton('2', callback_data='2'),
            telebot.types.InlineKeyboardButton('3', callback_data='3'))
 
-def add_new_polling(chat_id,message_id,message_text):
+
+def add_new_polling(chat_id, message_id, message_text):
     query = """SELECT *
         	        FROM public.polls
                     WHERE msg_id={};"""
@@ -28,14 +27,16 @@ def add_new_polling(chat_id,message_id,message_text):
         query = """INSERT INTO public.polls
                     (chat_id, msg_id,votes,sum,text,date)
 	         VALUES ({}, {},0,0,'{}',to_timestamp('{}','YYYY-MM-DD HH24:MI:SS'));"""
-        query_result = db_query.execute_query(query.format(chat_id, message_id,message_text,datetime.now()), is_dml=True)
+        query_result = db_query.execute_query(query.format(chat_id, message_id, message_text, datetime.now()),
+                                              is_dml=True)
         create_user_list(message_id)
-        add_new_polling(chat_id,message_id,message_text)
+        add_new_polling(chat_id, message_id, message_text)
     else:
         return query_result.value
 
+
 def create_user_list(message_id):
-   query = """CREATE TABLE public."{}"
+    query = """CREATE TABLE public."{}"
             (user_id integer NOT NULL)
             WITH (
             OIDS = FALSE
@@ -43,31 +44,34 @@ def create_user_list(message_id):
             TABLESPACE pg_default;
             ALTER TABLE public."{}"
             OWNER to root;"""
-   query_result = db_query.execute_query(query.format(message_id,message_id), is_dml=True)
-   print('CREATE USER LIST')
-   print(query_result.success)
+    query_result = db_query.execute_query(query.format(message_id, message_id), is_dml=True)
+    print('CREATE USER LIST')
+    print(query_result.success)
 
-def user_is_new(message_id,user_id):
+
+def user_is_new(message_id, user_id):
     query = """SELECT *
             	        FROM public."{}"
                         WHERE user_id={};"""
-    query_result = db_query.execute_query(query.format(message_id,user_id))
+    query_result = db_query.execute_query(query.format(message_id, user_id))
     if len(query_result.value) < 1:
         query = """INSERT INTO public."{}"
                         (user_id)
     	         VALUES ({});"""
-        query_result = db_query.execute_query(query.format(message_id,user_id), is_dml=True)
+        query_result = db_query.execute_query(query.format(message_id, user_id), is_dml=True)
         return True
     else:
         return False
 
-def add_vote(message_id,votes):
+
+def add_vote(message_id, votes):
     query = """UPDATE public.polls
 	            SET votes={}
 	            WHERE msg_id={};"""
-    query_result = db_query.execute_query(query.format(int(votes)+1,message_id), is_dml=True)
+    query_result = db_query.execute_query(query.format(int(votes) + 1, message_id), is_dml=True)
 
-def new_sum(message_id,new_summa):
+
+def new_sum(message_id, new_summa):
     query = """UPDATE public.polls
     	            SET sum={}
     	            WHERE msg_id={};"""
@@ -80,8 +84,9 @@ def test(message):
                  "Роли участников проектной команды распределены, не дублируются и работа ведется в соответствии с этими ролями",
                  "У участников проекта есть понятные роли, они не дублируется и непокрытых зон также нет"]
     for msg in questions:
-        bot.send_message(message.chat.id, msg ,reply_markup=markup)
+        bot.send_message(message.chat.id, msg, reply_markup=markup)
     pass
+
 
 # @bot.message_handler(regexp="/all_result")
 # def all_result(message):
@@ -96,12 +101,66 @@ def test(message):
 @bot.message_handler(regexp="/radushin")
 def test(message):
     working_directory = os.path.dirname(os.path.abspath(__file__))
-    script_path =  working_directory + '/try.py'
-    subprocess.Popen(script_path)
+    workbook = Workbook()
+    query = """SELECT *
+        	        FROM public.polls
+        	        ORDER by chat_id ASC, id ASC;"""
+    query_result = db_query.execute_query(query)
+    temp_chat_name = ''
+    # chat_names = []
+    for data in query_result.value:
+        # print(data)
+        # print(data[1])
+        # print(bot.get_chat(data[1]).title)
+        if temp_chat_name == bot.get_chat(data[1]).title:
+            if i % 3 == 0:
+                work_sheet['A' + str(row)] = str(data[6])
+                work_sheet['B' + str(row)] = str(data[4]).replace('.', ',')
+                work_sheet['C' + str(row)] = str(data[3])
+                i = i + 1
+                continue
+            elif i % 3 == 1:
+                work_sheet['D' + str(row)] = str(data[4]).replace('.', ',')
+                work_sheet['E' + str(row)] = str(data[3])
+                i = i + 1
+                continue
+            elif i % 3 == 2:
+                work_sheet['F' + str(row)] = str(data[4]).replace('.', ',')
+                work_sheet['G' + str(row)] = str(data[3])
+                i = i + 1
+                row = row + 1
+                continue
+        else:
+            # chat_names.append(bot.get_chat(data[1]).title)
+            work_sheet = workbook.create_sheet(bot.get_chat(data[1]).title)
+            work_sheet['B1'] = 'Question1'
+            work_sheet['C1'] = 'Voters1'
+            work_sheet['D1'] = 'Question2'
+            work_sheet['E1'] = 'Voters2'
+            work_sheet['F1'] = 'Question3'
+            work_sheet['G1'] = 'Voters3'
+            work_sheet['A2'] = str(data[6])
+            work_sheet['B2'] = str(data[4]).replace('.', ',')
+            work_sheet['C2'] = str(data[3])
+            temp_chat_name = bot.get_chat(data[1]).title
+            i = 1
+            row = 2
+    # print(chat_names)
+    #
+    # for name in chat_names:
+    #     workbook.create_sheet(name)
+
+    workbook.save('test.xlsx')
+
+    z = zipfile.ZipFile('test.zip', 'w')
+    z.write('test.xlsx')
+    z.close()
+
     # test_file = open(, 'rb')
-    bot.send_document(message.chat.id, open(working_directory+'/test.zip', 'rb'))
+    bot.send_document(message.chat.id, open(working_directory + '/test.zip', 'rb'))
     # bot.send_document(message.chat.id,working_directory+'/test.zip')
     pass
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
@@ -110,24 +169,25 @@ def callback_inline(call):
             # bot.answer_callback_query(call.id, text="Done!")
             # user_id = json.loads(call.message)[0]
             print(1)
-            data = add_new_polling(call.message.chat.id,call.message.message_id,call.message.text)
+            data = add_new_polling(call.message.chat.id, call.message.message_id, call.message.text)
             try:
                 votes = data[0][3]
                 summa = data[0][4]
             except:
-                votes=0
-                summa=0
+                votes = 0
+                summa = 0
             print('votes ' + str(votes))
             print('summa ' + str(summa))
             if user_is_new(call.message.message_id, call.from_user.id):
-                new_summa = (int(votes)*float(summa) + int(call.data))/(int(votes)+1)
-                add_vote(call.message.message_id,votes)
-                new_sum(call.message.message_id,new_summa)
+                new_summa = (int(votes) * float(summa) + int(call.data)) / (int(votes) + 1)
+                add_vote(call.message.message_id, votes)
+                new_sum(call.message.message_id, new_summa)
                 bot.answer_callback_query(call.id, text=str(call.data))
             else:
                 print(2)
                 bot.answer_callback_query(call.id, text='Already voted')
     pass
+
 
 @bot.message_handler(regexp="/result")
 def get_result(message):
@@ -143,14 +203,13 @@ def get_result(message):
     Среднее значение: {} \n
             """
     for data in query_result.value:
-        bot.send_message(message.chat.id, msg.format(data[5],data[3],data[4]))
+        bot.send_message(message.chat.id, msg.format(data[5], data[3], data[4]))
 
 
 @bot.message_handler(content_types='text')
 def default_answer(message):
     # bot.send_message(message.chat.id, "You are not authorized")
     pass
-
 
 
 while True:
