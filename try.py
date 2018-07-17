3e3f42af7a77f0a495c361a57ba86db8f776a1ba
Arseny Radushin <radushin.arseny@yandex.ru>
2018-07-17 21:56:00 +0300

    Try new result

--- try.py
+++ try.py
@@ -13,17 +13,30 @@ query = """SELECT *
     	        ORDER by chat_id ASC, id ASC;"""
 query_result = db_query.execute_query(query)
 temp_chat_name = ''
-chat_names = []
-i = 0
+# chat_names = []
 for data in query_result.value:
     # print(data)
     # print(data[1])
     # print(bot.get_chat(data[1]).title)
     if temp_chat_name ==bot.get_chat(data[1]).title:
-
-        continue
+        if i % 3 == 0 :
+            work_sheet['B' + str(row)] = str(data[4]).replace('.', ',')
+            work_sheet['C' + str(row)] = str(data[3])
+            i = i + 1
+            continue
+        elif i % 3 == 1:
+            work_sheet['D'+str(row)] = str(data[4]).replace('.',',')
+            work_sheet['E' + str(row)] = str(data[3])
+            i= i +1
+            continue
+        elif i % 3 == 2:
+            work_sheet['F' + str(row)] = str(data[4]).replace('.', ',')
+            work_sheet['G' + str(row)] = str(data[3])
+            i = i +1
+            row = row +1
+            continue
     else:
-        chat_names.append(bot.get_chat(data[1]).title)
+        # chat_names.append(bot.get_chat(data[1]).title)
         work_sheet = workbook.create_sheet(bot.get_chat(data[1]).title)
         work_sheet['B1'] = 'Question1'
         work_sheet['C1'] = 'Voters1'
@@ -35,6 +48,8 @@ for data in query_result.value:
         work_sheet['B2'] = str(data[4]).replace('.',',')
         work_sheet['C2'] = str(data[3])
         temp_chat_name = bot.get_chat(data[1]).title
+        i=1
+        row = 2
 # print(chat_names)
 #
 # for name in chat_names:
